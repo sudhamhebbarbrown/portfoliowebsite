c881cbbc6d42633b851ad07e83ce3c7f68719c00
sudhamhebbarbrown <sudham_hebbar@brown.edu>
2024-10-19 13:33:33 -0400

    Fixed typos and added bios

--- app/page.tsx
+++ app/page.tsx
@@ -37,32 +37,35 @@ export default function Home() {
   return (
     <>
       <AnimatedName />
-      <div className="text-gray-800 space-y-4 leading-snug">
+      <div className="space-y-4 leading-snug">
         <p>
-          I'm a full stack developer and a distributed systems engineer. I am a  at{' '}
+          I'm a full stack developer and a distributed systems engineer. I am a {' '}
           <Link href="/education" className="text-blue-500 hover:text-blue-700">
             student
           </Link>
-          , Brown University here my primary concencration is distributed systems and programming languages.
+          , at Brown University where my primary focus is distributed systems and databases.
         </p>
         <p>
           I have previously{' '}
           <Link href="/work" className="text-blue-500 hover:text-blue-700">
-          worked
+            worked
           </Link>
-         {' '} at {'Schneider Electric'} as a backend developer and eClinicalworks as a software intern.
+          {' '} at {'Schneider Electric'} as a backend developer and eClinicalworks as a software intern.
         </p>
         <p>
           I have worked on many{' '}
           <Link href="/projects" className="text-blue-500 hover:text-blue-700">
-          projects
-          </Link> 
-         {' '} including a distributed search engine, a multi-threaded database system, and a music streaming service.
+            projects
+          </Link>
+          {' '} including a distributed search engine, a multi-threaded database system, and a music streaming service.
         </p>
         <p>
-          I am currently researching on combined static and dynamic analysis to prevent supply chain attacks in npm packages.
+          I am currently researching on combined static and dynamic analysis to prevent supply chain attacks in npm packages. If you're looking to collaborate on research/projects, feel free to ping me.
         </p>
-        <div className="bg-gray-100 border-l-4 border-blue-500 text-gray-700 p-4 my-4"><p>🚨 I am <b>open to work</b> (preferably backend engineering), so if you think you'd like a hardworking &amp; passionate engineer on your team, please reach out!🚨</p></div>
+        <p>
+          Most of software principles I follow are inspired by the {'Designing Data-Intensive Applications'} book by {'Martin Kleppmann'} and from the teachings of industry experts I meet at the {'NYC systems meetup'} hosted by {'Phil Eaton'}.
+        </p>
+        <div className="bg-gray-300 border-l-4 border-blue-500 text-gray-800 p-4 my-4"><p>🚨 I am <b>open to work</b> (preferably backend engineering), so if you think you'd like a hardworking &amp; passionate engineer on your team, please reach out!🚨</p></div>
       </div>
     </>
   );
